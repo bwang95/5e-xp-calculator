db5bb08c063d8538be0972b21064659f41eed548
Brian Wang <b.wang@utexas.edu>
2021-01-01 17:54:09 -0800

    Add documentation to new ResultEpoxyModel subclasses

--- app/src/main/java/com/cerridan/dndxpcalc/adapter/result/ResultEpoxyModel.kt
+++ app/src/main/java/com/cerridan/dndxpcalc/adapter/result/ResultEpoxyModel.kt
@@ -12,7 +12,19 @@ import androidx.annotation.StringRes
  * @since December 31st, 2020
  */
 sealed class ResultEpoxyModel(val id: String) {
+
+  /**
+   * Header item.
+   *
+   * @see com.cerridan.dndxpcalc.ui.HeaderItemView
+   * @param header header text resource to display.
+   */
   class HeaderItem(@StringRes val header: Int) : ResultEpoxyModel(header.toString())
 
+  /**
+   * Key-value item for displaying results.
+   *
+   * @see com.cerridan.dndxpcalc.ui.ResultItemView
+   */
   class ResultItem(@StringRes val title: Int, val value: String) : ResultEpoxyModel(title.toString())
 }
